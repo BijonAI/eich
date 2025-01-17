c1963b11acc02d1920d6d794f3dd3f1ea6551cf4
Takamatsu Tomori <168127639+tomoriny@users.noreply.github.com>
2025-01-17 21:22:00 +0800

    ci: sync

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -2,4 +2,8 @@ export * from './html'
 export * from './parser'
 export * from './renderer'
 export * from './utils'
+
+// !TODO: remove
+export * from './intrinisics'
+
 export * from '@vue/reactivity'
