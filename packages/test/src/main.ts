777f26658526302d4b0f98f6a4e6e52e75df5fd9
Takamatsu Tomori <168127639+tomoriny@users.noreply.github.com>
2025-01-22 00:34:50 +0800

    ci: sync

--- packages/test/src/main.ts
+++ packages/test/src/main.ts
@@ -1,9 +1,9 @@
-import { eich, parse, parseRaw, renderRoots } from "@eich/renderer"
+import { parse, parseRaw } from "@eich/renderer"
 
-const app = document.querySelector<HTMLDivElement>('#app')!
 const source = `
 <value $data="x + y" />
 {{ x + y }}
 `
 
-console.log(parse(source), parseRaw(source))
+console.log(parse(source))
+console.log(parseRaw(source))
