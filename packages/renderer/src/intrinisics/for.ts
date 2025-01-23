b06d55b333fbaf1d77a3c4cc65543697333bb69c
Acbox <850625057@qq.com>
2025-01-23 12:24:55 +0800

    fix: tsconfig paths

--- packages/renderer/src/intrinisics/for.ts
+++ packages/renderer/src/intrinisics/for.ts
@@ -1,4 +1,4 @@
-import { effect, type EffectScope, effectScope, readonly } from '@vue/reactivity'
+import { type EffectScope, effect, effectScope, readonly } from '@vue/reactivity'
 import {
   createAdhoc,
   defineComponent,
--- packages/renderer/tsup.config.ts
+++ packages/renderer/tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts', 'src/intrinisics/*.ts'],
   format: 'esm',
-  tsconfig: '../../tsconfig.json',
-  globalName: 'vuemotion',
+  tsconfig: './tsconfig.json',
+  globalName: 'eich',
   splitting: true,
   clean: true,
   dts: true,
