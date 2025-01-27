f90536cf8c5bf833ee72958a7196580785a27f98
Takamatsu Tomori <168127639+tomoriny@users.noreply.github.com>
2025-01-27 18:01:03 +0800

    ci: sync

--- packages/animation/src/animate.ts
+++ packages/animation/src/animate.ts
@@ -2,16 +2,10 @@
 
 const n = performance.now.bind(performance) ?? Date.now
 const r = requestAnimationFrame ?? (x => setTimeout(x, 16.67))
-
 export function animate(f: (x: number) => void, d: number, t?: (x: number) => number): Promise<void> {
-  let e: number, x: number
   return new Promise((c) => {
-    const g = () => {
-      x = e + n() / d
-      x = x > 1 ? 1 : (r(g), x < 0 ? 0 : x)
-      f(t ? t(x) : x)
-      x < 1 || c()
-    }
+    let e: number, x: number
+    const g = () => (x = e + n() / d, x = x > 1 ? 1 : (r(g), x < 0 ? 0 : x), f(t ? t(x) : x), x < 1 || c())
     r(() => (e = n() / -d, g()))
   })
 }
