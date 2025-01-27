1a46bd52f7114364d23362ccaa2b5a8478e6c463
Takamatsu Tomori <168127639+tomoriny@users.noreply.github.com>
2025-01-27 18:18:05 +0800

    fix(animation): animate

--- packages/animation/src/animate.ts
+++ packages/animation/src/animate.ts
@@ -6,8 +6,8 @@ const n = performance.now.bind(performance) ?? Date.now
 const r = requestAnimationFrame ?? (x => setTimeout(x, 16.67))
 function __animate(a, d, t, o) {
   let e, x
-  const g = () => (x = e + n() / d, a(x >= 1 ? (o && o(), 1) : (r(g), t ? t(x) : x)))
-  r(() => (e = n() / -d, g()))
+  const g = () => (x = (e + n()) / d, a(x >= 1 ? (o && o(), 1) : (r(g), t ? t(x) : x)))
+  r(() => (e = -n(), g()))
 }
 
 export function animate(executor: (x: number) => void, duration: number, timing?: (x: number) => number): Promise<void> {
