d81944aa656d04b1376e26fc6c9ca49cafc96190
Takamatsu Tomori <168127639+tomoriny@users.noreply.github.com>
2025-01-27 17:49:36 +0800

    refactor(animation): animate fn

--- packages/animation/src/animate.ts
+++ packages/animation/src/animate.ts
@@ -1,12 +1,17 @@
-export async function animate(fn: (progress: number) => void, dur: number, ease: (t: number) => number): Promise<void> {
-  let elapsed = 0
-  const startTime = performance.now()
-  function updater() {
-    elapsed = performance.now() - startTime
-    fn(ease(elapsed / dur))
-    if (elapsed >= dur)
-      return
-    requestAnimationFrame(updater)
-  }
-  requestAnimationFrame(updater)
+/* eslint-disable no-sequences */
+
+const n = performance.now.bind(performance) ?? Date.now
+const r = requestAnimationFrame ?? (x => setTimeout(x, 16.67))
+
+export function animate(f: (x: number) => void, d: number, t: (x: number) => number): Promise<void> {
+  let e: number, x: number
+  return new Promise((c) => {
+    const g = () => {
+      x = e + n() / d
+      x = x > 1 ? 1 : (r(g), x < 0 ? 0 : x)
+      f(t(x))
+      x < 1 || c()
+    }
+    r(() => (e = n() / -d, g()))
+  })
 }
