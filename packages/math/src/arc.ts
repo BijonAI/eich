ac8beaf242fdd2182395236e6208f0fcbee33bde
Artist-MOBAI <mobai@duck.com>
2025-02-08 12:10:10 +0800

    feat: arc

--- packages/math/src/arc.ts
+++ packages/math/src/arc.ts
@@ -1,19 +1,57 @@
-import { builtins, defineComponent, toValue, useAttrs } from '@eich/renderer'
+import { builtins, defineComponent, effect, toValue, useAttrs } from '@eich/renderer'
 import { arc } from 'idea-math'
 
 export interface ArcAtrributes {
-  $from: number
-  $to: number
-  $radius: number
+  '$arc-from': number
+  '$arc-to': number
+  '$arc-radius': number
+  '$set-unit': number
+  '$from': number
+  '$to': number
+  '$info': boolean
+
+  '$origin': [number, number]
+  '$skew': [number, number]
+  '$scale': number
+  '$rotate': number
+  '$translate': [number, number]
 }
 
 const component = defineComponent<ArcAtrributes>((props) => {
-  const { from, to, radius } = useAttrs(props, ['from', 'to', 'radius'])
+  const {
+    'arc-from': arcFrom,
+    'arc-to': arcTo,
+    'arc-radius': arcRadius,
+    'set-unit': setUnit,
+    origin,
+    skew,
+    scale,
+    rotate,
+    translate,
+  } = useAttrs(props, ['arc-from', 'arc-to', 'arc-radius', 'set-unit', 'origin', 'skew', 'scale', 'rotate', 'translate', 'draggable'])
   const a = arc(
-    toValue(from) as unknown as number,
-    toValue(to) as unknown as number,
-    toValue(radius) as unknown as number,
+    toValue(arcFrom) as unknown as number,
+    toValue(arcTo) as unknown as number,
+    toValue(arcRadius) as unknown as number,
   )
+  effect(() => {
+    a.setUnit(toValue(setUnit) as unknown as number)
+    a.transform({
+      origin: toValue(origin) as unknown as [number, number],
+    })
+      .transform({
+        skew: toValue(skew) as unknown as [number, number],
+      })
+      .transform({
+        scale: toValue(scale) as unknown as number,
+      })
+      .transform({
+        rotate: toValue(rotate) as unknown as number,
+      })
+      .transform({
+        translate: toValue(translate) as unknown as [number, number],
+      })
+  })
   return a.node()
 })
 
