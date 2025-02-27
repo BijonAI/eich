9bd31fea41c733685d46b7614a07171afced3cfb
Acbox <me@acbox.lol>
2025-02-27 15:52:30 +0800

    feat: table & table-header

--- packages/components/src/table/table.ts
+++ packages/components/src/table/table.ts
@@ -1,3 +1,21 @@
-export interface TableAttributes {
+import { builtins, defineComponent, effect, toValue, useAttrs } from "@eichjs/renderer"
 
+export interface TableAttributes {
+  $border: string
+  '$border-width': number
 }
+
+const component = defineComponent<TableAttributes>((attrs, children) => {
+  const { border, 'border-width': borderWidth } = useAttrs(attrs, ['border', 'border-width'])
+  const table = document.createElement('table')
+  effect(() => {
+    table.style.border = toValue(border) ?? 'none'
+    table.style.borderWidth = toValue(borderWidth) ?? '0'
+  })
+  table.append(...children())
+
+  return table
+})
+
+builtins.set('table', component)
+export default component
