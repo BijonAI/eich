40ec453f5c1f669fd31ec7ba2ec89ed6b965bf8c
Neo Wang <wiirhan@163.com>
2025-03-03 22:49:43 +0800

    feat: add reactive styling and comprehensive attributes to tr component

--- packages/components/src/table/tr.ts
+++ packages/components/src/table/tr.ts
@@ -1,12 +1,36 @@
-import { builtins, defineComponent } from '@eichjs/renderer'
+import { builtins, defineComponent, effect, toValue, useAttrs } from '@eichjs/renderer'
 
 export interface TrAttributes {
-  // tr specific attributes can be added here if needed
+  // Box model attributes
+  '$padding': CSSStyleDeclaration['padding']
+  '$border': CSSStyleDeclaration['border']
+  '$text-align': CSSStyleDeclaration['textAlign']
+  '$vertical-align': CSSStyleDeclaration['verticalAlign']
+  '$width': CSSStyleDeclaration['width']
+  '$height': CSSStyleDeclaration['height']
+  '$background-color': CSSStyleDeclaration['backgroundColor']
+  '$color': CSSStyleDeclaration['color']
+  '$font-size': CSSStyleDeclaration['fontSize']
+  '$font-weight': CSSStyleDeclaration['fontWeight']
 }
 
 const component = defineComponent<TrAttributes>((attrs, children) => {
+  const props = useAttrs(attrs, ['padding', 'border', 'text-align', 'vertical-align', 'width', 'height', 'background-color', 'color', 'font-size', 'font-weight'])
   const tr = document.createElement('tr')
   tr.append(...children())
+
+  effect(() => {
+    tr.style.padding = toValue(props.padding) ?? ''
+    tr.style.border = toValue(props.border) ?? ''
+    tr.style.textAlign = toValue(props['text-align']) ?? ''
+    tr.style.verticalAlign = toValue(props['vertical-align']) ?? ''
+    tr.style.width = toValue(props.width) ?? ''
+    tr.style.height = toValue(props.height) ?? ''
+    tr.style.backgroundColor = toValue(props['background-color']) ?? ''
+    tr.style.color = toValue(props.color) ?? ''
+    tr.style.fontSize = toValue(props['font-size']) ?? ''
+    tr.style.fontWeight = toValue(props['font-weight']) ?? ''
+  })
 
   return tr
 })
